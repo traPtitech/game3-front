3dc9bbfa808baeeaa82e7b8f1cda52d9b9d5a577
eyemono-moe <ysdr83@gmail.com>
2023-12-01 18:55:33 +0900

    :sparkles: 作品応募ページで申し込み対象情報を記載

--- pages/entry/register.vue
+++ pages/entry/register.vue
@@ -1,55 +1,65 @@
 <!-- `/entry/register` -->
+<script setup lang="ts">
+const { data, isPending } = useCurrentEventQuery()
+</script>
+
 <template>
   <div>
-    <h2>第n回 GAME<sup>3</sup>展示作品登録</h2>
-    <div>
-      申し込み対象
-      <p>
-        第n回 GAME<sup>3</sup>
-        2023年X月Y日(Z曜日) 東京工業大学 大岡山キャンパス
-      </p>
+    <h2>GAME<sup>3</sup>展示作品登録</h2>
+    <div v-if="!isPending">
+      <div v-if="data">
+        <div>
+          申し込み対象：{{ data.title }} GAME<sup>3</sup>
+        </div>
+        <form class="flex flex-col gap-4">
+          <label>
+            ブース展示者名
+            <input type="text" required>
+          </label>
+          <label>
+            所属団体名
+            <input type="text">
+          </label>
+          <label>
+            参加予定人数
+            <input type="number" required>
+          </label>
+          <label>
+            作品タイトル
+            <input type="text" required>
+          </label>
+          <label>
+            作品ジャンル
+            <input type="text">
+          </label>
+          <label>
+            開発環境
+            <input type="text">
+          </label>
+          <label>
+            作品の説明
+            <input type="text">
+          </label>
+          <label>
+            紹介画像
+            <input type="file" accept="image/*">
+          </label>
+          <label>
+            X アカウント
+            <input type="text">
+          </label>
+          <label>
+            WebサイトURL
+            <input type="text">
+          </label>
+        </form>
+      </div>
+      <div v-else>
+        申し込み対象のイベントがありません
+      </div>
     </div>
-    <form>
-      <label>
-        ブース展示者名
-        <input type="text" required>
-      </label>
-      <label>
-        所属団体名
-        <input type="text">
-      </label>
-      <label>
-        参加予定人数
-        <input type="number" required>
-      </label>
-      <label>
-        作品タイトル
-        <input type="text" required>
-      </label>
-      <label>
-        作品ジャンル
-        <input type="text">
-      </label>
-      <label>
-        開発環境
-        <input type="text">
-      </label>
-      <label>
-        作品の説明
-        <input type="text">
-      </label>
-      <label>
-        紹介画像
-        <input type="file" accept="image/*">
-      </label>
-      <label>
-        X アカウント
-        <input type="text">
-      </label>
-      <label>
-        WebサイトURL
-        <input type="text">
-      </label>
-    </form>
+    <div v-else>
+      Loading
+    </div>
   </div>
 </template>
