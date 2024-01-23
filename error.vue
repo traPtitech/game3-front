45d6855929965c1c69ff38c03c4ade0c2865bc66
eyemono-moe <ysdr83@gmail.com>
2024-01-24 03:58:16 +0900

    Add error.vue component

---
+++ error.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<{
+  error: {
+    url: string;
+    statusCode: number;
+    statusMessage: string;
+    message: string;
+    description: string;
+    data: any;
+  };
+}>()
+</script>
+
+<template>
+  <div>
+    <h1>{{ props.error.statusCode }}</h1>
+    <NuxtLink to="/">
+      Go back home
+    </NuxtLink>
+  </div>
+</template>
