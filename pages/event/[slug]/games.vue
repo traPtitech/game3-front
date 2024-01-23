1edc8f071c793b851ec368452e759ce9f3e73fd0
eyemono-moe <ysdr83@gmail.com>
2024-01-24 04:03:13 +0900

    Refactor error handling in games.vue

--- pages/event/[slug]/games.vue
+++ pages/event/[slug]/games.vue
@@ -5,22 +5,27 @@ import { getParamsArray } from '~/lib/url'
 const route = useRoute()
 const slug = getParamsArray(route.params.slug)
 if (!slug) {
-  throw new Error('slug is not found')
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'ゲームが見つかりませんでした'
+  })
 }
 
-// TODO: not found error handling
-const { data, status, suspense } = useGamesQuery(slug[0])
-
+const { data, error, suspense } = useEventGamesQuery(slug[0])
 onServerPrefetch(async () => {
   await suspense()
 })
+
+if (error) {
+  throw createError({
+    statusCode: 500,
+    statusMessage: error.value?.message
+  })
+}
 </script>
 
 <template>
-  <div v-if="status === 'pending'">
-    <div>読み込み中</div>
-  </div>
-  <div v-else-if="data">
+  <div>
     <h2>{{ slug[0] }}ゲーム一覧</h2>
     <ul>
       <li v-for="game in data" :key="game.id">
