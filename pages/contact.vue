25f12cd7f247405d957703ebbb623f039d30b0c2
eyemono-moe <ysdr83@gmail.com>
2024-01-31 08:29:20 +0900

    Add contact page

---
+++ pages/contact.vue
@@ -0,0 +1,34 @@
+<!-- `/contact` -->
+<template>
+  <div class="h-full flex flex-col items-center px-8 pb-16 pt-4">
+    <div class="max-w-1000px w-full flex flex-col gap-4">
+      <h2 class="text-center text-h2 text-text-white text-shadow-4">
+        <StrokedText>
+          お問い合わせフォーム
+        </StrokedText>
+      </h2>
+      <div>
+        イベント全体についてのご質問・ご相談は以下のフォームより、「その他お問い合わせ」をご選択の上、お問い合わせ内容の冒頭に『【Game3に関する問い合わせ】』を付記いただくようお願いします。
+      </div>
+      <div class="h-full w-full flex justify-center">
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSdzEZBKnQP21iuczfuG22zD5_1ZPzeWOsmhFs_Iqc-OYLtYkg/viewform?embedded=true"
+          height="1000px"
+          class="max-w-800px w-full"
+        >
+          読み込んでいます…</iframe>
+      </div>
+      <h2 class="text-center text-h2 text-text-white text-shadow-4">
+        <StrokedText>
+          その他のお問い合わせ先
+        </StrokedText>
+      </h2>
+      <div>
+        運営やイベント開催についてのお問い合わせはDiscordサーバーでも受け付けております。
+        サーバーへの参加リンク: <a href="https://discord.gg/xxxxx">
+          https://discord.gg/xxxxx
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
