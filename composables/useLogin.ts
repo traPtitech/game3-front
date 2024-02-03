af5a337630e7ab7e746402b4e8bf01a69289c613
eyemono-moe <ysdr83@gmail.com>
2024-02-03 14:47:05 +0900

    Fix redirect issue in useLogin.ts

--- composables/useLogin.ts
+++ composables/useLogin.ts
@@ -4,10 +4,11 @@ import { useMe } from '~/store/me'
 export const useLogin = () => {
   const { useMeStore, logout } = useMe()
   const { redirectPath } = useRedirectParam()
+  const config = useRuntimeConfig()
 
   const login = async () => {
     await authApi.login({
-      redirect: redirectPath.value ?? '/'
+      redirect: encodeURI(config.public.basePath + (redirectPath.value ?? '/'))
     })
   }
 
