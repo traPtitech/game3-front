ed4d8bdccf5df7115966636a54c9f9248463d78e
eyemono-moe <ysdr83@gmail.com>
2024-02-03 20:03:17 +0900

    Refactor isAdmin function in needAdmin middleware

--- middleware/needAdmin.ts
+++ middleware/needAdmin.ts
@@ -1,11 +1,15 @@
 const isAdmin = () => {
   const { useMeStore } = useLogin()
   const me = useMeStore()
-  return me.value.user?.role === 'admin'
+  return me.value.user ? me.value.user.role === 'admin' : undefined
 }
 
-export default defineNuxtRouteMiddleware(() => {
-  if (!isAdmin()) {
+const loginPage = '/login'
+
+export default defineNuxtRouteMiddleware((to) => {
+  if (isAdmin() === undefined) {
+    return navigateTo(loginPage + '?redirect=' + encodeURIComponent(to.path))
+  } else if (!isAdmin()) {
     throw createError({
       statusCode: 403,
       statusMessage: 'Forbidden'
