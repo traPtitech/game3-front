eb018aaf60d8fdd6ff36a3db2aad2705a423e420
eyemono-moe <ysdr83@gmail.com>
2024-02-04 14:16:48 +0900

    wip: fix content 404

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,13 +1,18 @@
 <!-- `/`以下の全てのパスにマッチ -->
 <!-- see: https://nuxt.com/docs/guide/directory-structure/pages#catch-all-route -->
+<script setup>
+const { path } = useRoute()
+
+const { data, error } = await useAsyncData(`content-${path}`, () => {
+  return queryContent().where({ _path: path }).findOne()
+})
+if (error.value) {
+  showError(error.value)
+}
+</script>
+
 <template>
   <main>
-    <!-- `/content`以下のMarkdownファイルを表示 -->
-    <ContentDoc>
-      <template #not-found>
-        <!-- マッチするMarkdownファイルがない場合は404ページを表示 -->
-        <ErrorPage />
-      </template>
-    </ContentDoc>
+    <ContentRenderer :value="data" />
   </main>
 </template>
