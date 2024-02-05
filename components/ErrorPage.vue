2fa15526dd9d79dd50f85d2c7c2308132649e555
eyemono-moe <ysdr83@gmail.com>
2024-02-05 17:04:14 +0900

    Refactor error page component

--- components/ErrorPage.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <p>Page not found</p>
-  <h1>ページが見つかりませんでした＞＜</h1>
-  <iframe src="https://buriclicker.trap.games/" style="height:600px; width:100%;" loading="lazy" />
-  <p>
-    Buri Clicker by <a href="https://twitter.com/deka0106">@deka0106</a>
-  </p>
-</template>
---
+++ error.vue
@@ -0,0 +1,105 @@
+<script setup lang="ts">
+const props = defineProps<{
+  error: {
+    url: string;
+    statusCode: number;
+    statusMessage: string;
+    message: string;
+    description: string;
+    data: any;
+  };
+}>()
+</script>
+
+<template>
+  <div grid min-h-screen class="grid-rows-[auto_1fr_auto]">
+    <div class="sticky top-0 z-1 shadow">
+      <header class="bg-surface-primary">
+        <nav class="flex items-center gap-4 px-4 py-2">
+          <NuxtLink to="/" class="mr-auto">
+            <picture>
+              <NuxtSource
+                src="/img/logo/Gamecube_symbol.svg"
+                media="(max-width: 320px)"
+                alt="Game³ロゴ"
+                class="h-16"
+              />
+              <NuxtImg
+                src="/img/logo/Gamecube_logo_full.svg"
+                alt="Game³ロゴ"
+                class="h-16"
+              />
+            </picture>
+          </NuxtLink>
+        </nav>
+      </header>
+    </div>
+    <main
+      class="h-full w-full flex flex-col items-center overflow-x-hidden px-4 pb-16 pt-0"
+    >
+      <div class="max-w-240 w-full">
+        <ProseH1>
+          {{ props.error.statusCode }} {{ props.error.statusMessage }}
+        </ProseH1>
+        <ProseP>
+          {{
+            props.error.statusCode === 404
+              ? "ページが見つかりませんでした"
+              : "予期せぬエラーが発生しました"
+          }}
+        </ProseP>
+        <ProseP>
+          <ProseA to="/">
+            トップページへ戻る
+          </ProseA>
+        </ProseP>
+        <iframe
+          src="https://buriclicker.trap.games/"
+          style="height: 600px; width: 100%"
+          loading="lazy"
+        />
+        <ProseP>
+          Buri Clicker by
+          <ProseA href="https://twitter.com/deka0106">
+            @deka0106
+          </ProseA>
+        </ProseP>
+      </div>
+    </main>
+    <footer
+      class="flex flex-col gap-4 bg-brand-violet px-4 py-6 text-text-white font-700 lg:(px-12 py-8) body"
+    >
+      <div class="flex items-center gap-2">
+        <a href="https://trap.jp" class="w-full">
+          <NuxtImg
+            src="/img/logo/traP_logo_full_white.svg"
+            alt="東京工業大学デジタル創作同好会traPロゴ"
+            class="h-auto max-w-full w-64 object-contain"
+          /></a>
+        <a
+          href="https://twitter.com/traPtitech"
+          class="ml-auto h-10 w-10 flex shrink-0 items-center justify-center p-2"
+        >
+          <NuxtImg
+            src="/img/logo/x_logo.svg"
+            alt="X(Twitter)ロゴ"
+          /></a>
+      </div>
+      <hr class="bg-surface-primary">
+      <div class="flex flex-col gap-2 lg:(flex-row gap-6)">
+        <NuxtLink to="/">
+          <StrokedText> トップページへ </StrokedText>
+        </NuxtLink>
+        <NuxtLink to="/contact">
+          <StrokedText> お問い合わせ </StrokedText>
+        </NuxtLink>
+        <NuxtLink to="/about-google-analytics">
+          <StrokedText> Googleアナリティクスについて </StrokedText>
+        </NuxtLink>
+        <StrokedText class="ml-auto">
+          © 2024 traP TokyoTech
+        </StrokedText>
+      </div>
+    </footer>
+  </div>
+</template>
