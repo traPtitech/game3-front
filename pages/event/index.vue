7b2bd05e3b81e21a7039a7dd68c6f9267657cbf2
eyemono-moe <ysdr83@gmail.com>
2024-02-06 08:58:23 +0900

    Refactor event list sorting

--- pages/event/index.vue
+++ pages/event/index.vue
@@ -5,6 +5,10 @@ onServerPrefetch(async () => {
   await suspenseEvents().catch(() => {})
 })
 
+const sortedEvents = computed(() => {
+  return events.value?.toSorted((a, b) => (a.date > b.date ? -1 : 1)) ?? []
+})
+
 useSeoMeta({
   title: '過去の開催',
   ogTitle: '過去の開催'
@@ -13,17 +17,11 @@ useSeoMeta({
 
 <template>
   <div>
-    <ProseH1>
-      過去の開催
-    </ProseH1>
-    <ProseH2>
-      開催イベント一覧
-    </ProseH2>
+    <ProseH1> 過去の開催 </ProseH1>
+    <ProseH2> 開催イベント一覧 </ProseH2>
     <ul class="divide-y-2">
-      <li v-for="event in events" :key="event.slug" class="py-5">
-        <EventRow
-          :event="event"
-        />
+      <li v-for="event in sortedEvents" :key="event.slug" class="py-5">
+        <EventRow :event="event" />
       </li>
     </ul>
   </div>
