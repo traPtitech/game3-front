a6543b0e4972d0e3d648b4ba11ad484297a5dff1
eyemono-moe <ysdr83@gmail.com>
2024-02-07 12:36:36 +0900

    wip: top loading animation

---
+++ components/LoadingIndicator.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="h-full w-full flex flex-col items-center justify-center gap-2">
+    <div
+      class="[mask:url('/img/loading-min.svg')_no-repeat_0_0/100%] aspect-415/256 h-auto max-w-full w-32 bg-current"
+    />
+    <div class="caption">
+      loading...
+    </div>
+  </div>
+</template>
--- layouts/hero.vue
+++ layouts/hero.vue
@@ -10,17 +10,16 @@ onServerPrefetch(async () => {
 
 const pageLoaded = useState('loaded', () => false)
 
-const onLoad = () => {
+onMounted(() => {
   pageLoaded.value = true
-}
-onMounted(onLoad)
+})
 </script>
 
 <template>
-  <div>
+  <div class="">
     <!-- TODO: add effect animation -->
     <div
-      class="grid grid-cols-[auto] grid-rows-[1fr_auto] h-100dvh w-full overflow-hidden lg:(grid-cols-[1fr_auto] grid-rows-[auto])"
+      class="grid grid-cols-[auto] grid-rows-[1fr_auto] h-100dvh w-full animate-duration-200 animate-ease-out overflow-hidden lg:(grid-cols-[1fr_auto] grid-rows-[auto])"
     >
       <div class="relative h-full w-full overflow-hidden">
         <NuxtImg
@@ -36,15 +35,66 @@ onMounted(onLoad)
           class="absolute bottom-0 right-0 max-w-500px w-full p-4 lg:w-500px"
         />
         <div
-          :class="['absolute bottom-0 right-0 h-full w-full bg-brand-violet transition-property-[width,height] duration-300 ease-out', {
-            'lg:(w-0! h-full!) h-0! w-full!': pageLoaded,
-          }]"
+          :class="[
+            'absolute bottom-0 right-0 w-full h-full bg-brand-orange',
+            {
+              '[--delay:1100ms] max-lg:animate-[300ms_var(--delay)_ease-out_forwards_slide-out-y] lg:animate-[300ms_var(--delay)_ease-out_forwards_slide-out-x]':
+                pageLoaded,
+            },
+          ]"
         />
+        <div
+          :class="[
+            'absolute bottom-0 right-0 w-full h-full bg-brand-violet',
+            {
+              '[--delay:1000ms] max-lg:animate-[300ms_var(--delay)_ease-out_forwards_slide-out-y] lg:animate-[300ms_var(--delay)_ease-out_forwards_slide-out-x]':
+                pageLoaded,
+            },
+          ]"
+        />
+        <div
+          v-if="!pageLoaded"
+          class="absolute inset-0"
+        >
+          <LoadingIndicator class="color-white" />
+        </div>
+        <div
+          v-else
+          class="[--delay:0ms] absolute inset-0 flex animate-[1000ms_var(--delay)_ease_forwards_show-hide] items-center justify-center"
+        >
+          <div
+            class="flex flex-col items-center justify-center gap-2 text-nowrap text-text-white h2-text"
+          >
+            <div
+              :class="[
+                'w-full b-l-4 b-white from-brand-cyan/80 to-brand-cyan/30 bg-gradient-to-r pl-4 pr-32 text-left text-brand-violet',
+                {
+                  '[--delay:0ms] animate-[300ms_var(--delay)_forwards_button-select]':
+                    pageLoaded,
+                },
+              ]"
+            >
+              New Game
+            </div>
+            <div
+              class="w-full b-l-4 b-white from-brand-cyan/20 to-transparent bg-gradient-to-r pl-4 pr-32"
+            >
+              Continue
+            </div>
+            <div
+              class="w-full b-l-4 b-white from-brand-cyan/20 to-transparent bg-gradient-to-r pl-4 pr-32"
+            >
+              Settings
+            </div>
+          </div>
+        </div>
       </div>
       <div
         class="h-full w-full flex flex-col items-end bg-brand-violet p-4 text-text-white sm:items-start lg:justify-center md:p-6"
       >
-        <div class="mb-2 break-keep break-anywhere text-right font-700 sm:text-left h4-text lg:h2-text sm:h3-text">
+        <div
+          class="mb-2 break-keep break-anywhere text-right font-700 sm:text-left h4-text lg:h2-text sm:h3-text"
+        >
           ゲーム制作者<wbr>交流イベント
         </div>
         <div
@@ -91,3 +141,7 @@ onMounted(onLoad)
     </div>
   </div>
 </template>
+
+<style lang="scss">
+@use "~/assets/css/top-page.scss";
+</style>
