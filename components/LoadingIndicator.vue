6d8dac34b911a069a947d5a832affa311d85f3e7
eyemono-moe <ysdr83@gmail.com>
2024-02-07 17:39:22 +0900

    add default color to loading indicator

--- components/LoadingIndicator.vue
+++ components/LoadingIndicator.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-full w-full flex flex-col items-center justify-center gap-2">
+  <div class="h-full w-full flex flex-col items-center justify-center gap-2 color-brand-violet">
     <div
       class="[mask:url('/img/loading-min.svg')_no-repeat_0_0/100%] aspect-415/256 h-auto max-w-full w-32 bg-current"
     />
