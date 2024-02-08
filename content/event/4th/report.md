c8b61a0164937fdebdfce8b9458e55c7c58d76f6
eyemono-moe <ysdr83@gmail.com>
2024-02-08 19:01:23 +0900

    過去開催回へのリダイレクト設定

--- content/event/4th/report.md
+++
@@ -1,9 +0,0 @@
-# 第3回 ゲーム制作者交流イベント GAME3
-
-第4回 GAME3 は、2016年9月3日(土)に開催されました。
-
-[イベント概要](http://seesaawiki.jp/gamecreatercon-game3/d/%a5%c8%a5%c3%a5%d7%a5%da%a1%bc%a5%b8)
-
-[ブース参加者一覧](http://seesaawiki.jp/gamecreatercon-game3/d/%a5%d6%a1%bc%a5%b9%bb%b2%b2%c3%bc%d4%b0%ec%cd%f7)
-
-当日の様子：[ロボット技術研究会の記事](http://titech-ssr.blog.jp/archives/1060795449.html)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,6 +42,46 @@ export default defineNuxtConfig({
     },
     '/me': {
       ssr: false
+    },
+    // old event page
+    '/event/1st': {
+      redirect: 'https://seesaawiki.jp/gamecreatercon-game3/d/%c2%e8%b0%ec%b2%f3%20GAME%5e3'
+    },
+    '/event/2nd': {
+      redirect: 'https://seesaawiki.jp/gamecreatercon-game3/d/%c2%e8%c6%f3%b2%f3%20GAME%5e3'
+    },
+    '/event/3rd': {
+      redirect: 'https://seesaawiki.jp/gamecreatercon-game3/d/%c2%e8%bb%b0%b2%f3%20GAME%5e3'
+    },
+    '/event/4th': {
+      redirect: 'https://seesaawiki.jp/gamecreatercon-game3/d/%c2%e8%bb%cd%b2%f3%20GAME%5e3'
+    },
+    '/event/5th': {
+      redirect: 'https://game3.trap.jp/5th/'
+    },
+    '/event/6th': {
+      redirect: 'https://game3.trap.jp/6th/'
+    },
+    '/event/7th': {
+      redirect: 'https://game3.trap.jp/7th/'
+    },
+    '/event/8th': {
+      redirect: 'https://game3.trap.jp/8th/'
+    },
+    '/event/9th': {
+      redirect: 'https://game3.trap.jp/9th/'
+    },
+    '/event/10th': {
+      redirect: 'https://game3.trap.jp/10th/'
+    },
+    '/event/15th': {
+      redirect: 'https://game3.trap.jp/15th/'
+    },
+    '/event/16th': {
+      redirect: 'https://game3.trap.jp/16th/'
+    },
+    '/event/17th': {
+      redirect: 'https://game3.trap.jp/17th/'
     }
   },
   build: {
