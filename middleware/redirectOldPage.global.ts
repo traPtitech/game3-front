83927ef3d14a0fd297749399b28d74ebb2e9e94b
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-08-22 17:42:45 +0900

    fix: 🐛 /event/21thへのアクセスを21stにリダイレクト

--- middleware/redirectOldPage.global.ts
+++ middleware/redirectOldPage.global.ts
@@ -1,58 +1,61 @@
 const redirectRules: {
-  [key: string]: {
-    redirect: string
-  }
+	[key: string]: {
+		redirect: string;
+	};
 } = {
-  '/event/1st': {
-    redirect:
-      'https://seesaawiki.jp/gamecreatercon-game3/d/%c2%e8%b0%ec%b2%f3%20GAME%5e3',
-  },
-  '/event/2nd': {
-    redirect:
-      'https://seesaawiki.jp/gamecreatercon-game3/d/%c2%e8%c6%f3%b2%f3%20GAME%5e3',
-  },
-  '/event/3rd': {
-    redirect:
-      'https://seesaawiki.jp/gamecreatercon-game3/d/%c2%e8%bb%b0%b2%f3%20GAME%5e3',
-  },
-  '/event/4th': {
-    redirect:
-      'https://seesaawiki.jp/gamecreatercon-game3/d/%c2%e8%bb%cd%b2%f3%20GAME%5e3',
-  },
-  '/event/5th': {
-    redirect: 'https://game3.trap.jp/5th/',
-  },
-  '/event/6th': {
-    redirect: 'https://game3.trap.jp/6th/',
-  },
-  '/event/7th': {
-    redirect: 'https://game3.trap.jp/7th/',
-  },
-  '/event/8th': {
-    redirect: 'https://game3.trap.jp/8th/',
-  },
-  '/event/9th': {
-    redirect: 'https://game3.trap.jp/9th/',
-  },
-  '/event/10th': {
-    redirect: 'https://game3.trap.jp/10th/',
-  },
-  '/event/15th': {
-    redirect: 'https://game3.trap.jp/15th/',
-  },
-  '/event/16th': {
-    redirect: 'https://game3.trap.jp/16th/',
-  },
-  '/event/17th': {
-    redirect: 'https://game3.trap.jp/17th/',
-  },
-}
+	"/event/1st": {
+		redirect:
+			"https://seesaawiki.jp/gamecreatercon-game3/d/%c2%e8%b0%ec%b2%f3%20GAME%5e3",
+	},
+	"/event/2nd": {
+		redirect:
+			"https://seesaawiki.jp/gamecreatercon-game3/d/%c2%e8%c6%f3%b2%f3%20GAME%5e3",
+	},
+	"/event/3rd": {
+		redirect:
+			"https://seesaawiki.jp/gamecreatercon-game3/d/%c2%e8%bb%b0%b2%f3%20GAME%5e3",
+	},
+	"/event/4th": {
+		redirect:
+			"https://seesaawiki.jp/gamecreatercon-game3/d/%c2%e8%bb%cd%b2%f3%20GAME%5e3",
+	},
+	"/event/5th": {
+		redirect: "https://game3.trap.jp/5th/",
+	},
+	"/event/6th": {
+		redirect: "https://game3.trap.jp/6th/",
+	},
+	"/event/7th": {
+		redirect: "https://game3.trap.jp/7th/",
+	},
+	"/event/8th": {
+		redirect: "https://game3.trap.jp/8th/",
+	},
+	"/event/9th": {
+		redirect: "https://game3.trap.jp/9th/",
+	},
+	"/event/10th": {
+		redirect: "https://game3.trap.jp/10th/",
+	},
+	"/event/15th": {
+		redirect: "https://game3.trap.jp/15th/",
+	},
+	"/event/16th": {
+		redirect: "https://game3.trap.jp/16th/",
+	},
+	"/event/17th": {
+		redirect: "https://game3.trap.jp/17th/",
+	},
+	"/event/21th": {
+		redirect: "https://game3.trap.games/event/21st/",
+	},
+};
 
 export default defineNuxtRouteMiddleware((to) => {
-  if (to.path in redirectRules) {
-    return navigateTo(redirectRules[to.path].redirect, {
-      external: true,
-      redirectCode: 302,
-    })
-  }
-})
+	if (to.path in redirectRules) {
+		return navigateTo(redirectRules[to.path].redirect, {
+			external: true,
+			redirectCode: 302,
+		});
+	}
+});
